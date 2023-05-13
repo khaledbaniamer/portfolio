7b0b5b5db8c62a81dfbc829d0c9140887e3a7a7b
Khaled bani amer <k.baniamer@hagzi.com>
2023-05-13 03:02:00 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+// import logo from './logo.svg';
+import './App.css';
+import Nav from './components/NavBar';
+import Header from './components/Hero';
+import About from './components/About';
+import Experience from './components/Experience';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+function App() {
+  const color = "teal";
+  return (
+    <>
+      <Nav color={color} />
+      <Header color={color} />
+      <About color={color} />
+      <Experience color={color} />
+      <Projects color={color} />
+      <Contact color={color} />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
